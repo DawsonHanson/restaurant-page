77c3ab7205e01dc519ad8106d491849299b0817a
Dawson Hanson <Dawson.Hanson2@gmail.com>
2022-12-22 11:22:44 -0700

    Added template layout & info to 'contact us' page

--- src/components.js
+++ src/components.js
@@ -61,6 +61,30 @@ export const menuItem = (name, info, price, imgSrc) => {
   content.appendChild(element)
 }
 
+export const contactInfoBlock = (name, position, number, email, imgSrc) => {
+  let element1 = document.createElement('div')
+  let element2 = document.createElement('div')
+  let p1 = document.createElement('p')
+  let p2 = document.createElement('p')
+  let p3 = document.createElement('p')
+  let h3 = document.createElement('h3')
+  let img = document.createElement('img')
+  p1.textContent = position
+  p2.textContent = number
+  p3.textContent = email
+  h3.textContent = name
+  img.src = imgSrc
+  element1.classList.add('place-holder')
+  element2.classList.add('contact-info')
+  element2.appendChild(p1)
+  element2.appendChild(p2)
+  element2.appendChild(p3)
+  element1.appendChild(h3)
+  element1.appendChild(element2)
+  element1.appendChild(img)
+  content.appendChild(element1)
+}
+
 export const render = () => {
   document.body.appendChild(content)
 }
--- src/contact-page.js
+++ src/contact-page.js
@@ -1,5 +1,13 @@
-import { heading } from "./components"
+import { heading, clearDOM, contactInfoBlock } from "./components"
+import tempImg from './imgs/food-template.png'
+
 export const contactPage = () => {
 
+  clearDOM()
+
   heading('Contact Us')
+  contactInfoBlock('Skarbrand', 'Chef', '(123) 444-5554', 'ExiledOne@RealEmail.com', tempImg)
+  contactInfoBlock('Cortana', 'Manager', '(123) 444-5555', 'MasterAI@RealEmail.com', tempImg)
+  contactInfoBlock('Yoshi', 'Waiter', '(123) 444-5556', 'GreenDinosaur@RealEmail.com', tempImg)
+  contactInfoBlock('Gordon Freeman', 'Maintenance', '(123) 444-5557', 'BlackMesaScientist@RealEmail.com', tempImg)
 }
